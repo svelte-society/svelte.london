4f793c48775b41a73a5e0f729822355e9b6553ef
spences10 <spences10apps@gmail.com>
2025-02-27 18:17:13 +0000

    chore: update community stats

--- src/lib/stats.ts
+++ src/lib/stats.ts
@@ -2,8 +2,8 @@ export const stats = [
 	{
 		in_person_range: '20-60',
 		online_viewers: '50-150',
-		youtube_subscribers: '17000',
-		community_members: '500',
+		youtube_subscribers: '24100',
+		community_members: '559',
 		founded_date: '2019', // Oct 1st 2019
 		monthly_active_members: '150',
 		average_talk_views: '2000',
